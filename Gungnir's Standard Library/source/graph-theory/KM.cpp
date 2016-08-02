efc610f6d3272596b1f721a36e275623b2392669
footoredo <footoredo@gmail.com>
2016-08-02 18:38:16 +0800

    update a lot

--- Gungnir's Standard Library/source/graph-theory/KM.cpp
+++ Gungnir's Standard Library/source/graph-theory/KM.cpp
@@ -4,40 +4,40 @@ struct KM {
 	int w[N][N];  
 	int lx[N], ly[N], match[N], way[N], slack[N];
 	bool used[N];
-	void initialization() {
-		for(int i = 1; i <= n; i++) {
+	void init() {
+		for (int i = 1; i <= n; i++) {
 			match[i] = 0;
 			lx[i] = 0;
 			ly[i] = 0;
 			way[i] = 0;
 		}
 	}
-	void hungary(int x) {  // for i(1 -> n) : hungary(i);
+	void hungary(int x) {
 		match[0] = x;
 		int j0 = 0;
-		for(int j = 0; j <= n; j++){
+		for (int j = 0; j <= n; j++) {
 			slack[j] = INF;
 			used[j] = false;
 		}
 		
 		do {
 			used[j0] = true;
-			int i0 = match[j0], delta = INF, j1;
-			for(int j = 1; j <= n; j++) {
-				if(used[j] == false) {
+			int i0 = match[j0], delta = INF, j1 = 0;
+			for (int j = 1; j <= n; j++) {
+				if (used[j] == false) {
 					int cur = -w[i0][j] - lx[i0] - ly[j];
-					if(cur < slack[j]) {
+					if (cur < slack[j]) {
 						slack[j] = cur;
 						way[j] = j0;
 					}
-					if(slack[j] < delta) {
+					if (slack[j] < delta) {
 						delta = slack[j];
 						j1 = j;
 					}
 				}
 			}
-			for(int j = 0; j <= n; j++) {
-				if(used[j]) {
+			for (int j = 0; j <= n; j++) {
+				if (used[j]) {
 					lx[match[j]] += delta;
 					ly[j] -= delta;
 				}
@@ -50,13 +50,15 @@ struct KM {
 			int j1 = way[j0];
 			match[j0] = match[j1];
 			j0 = j1;
-		} while(j0);
+		} while (j0);
 	}
 	
-	int get_ans() {  // maximum ans
+	int get_ans() {
 		int sum = 0;
-		for(int i = 1; i<= n; i++)
-			if(match[i] > 0) sum += -w[match[i]][i];
+		for(int i = 1; i <= n; i++) {
+			if (w[match[i]][i] == -INF) ; // 无解
+			if (match[i] > 0) sum += w[match[i]][i];
+		}
 		return sum;
 	}
-};
+} km;
