fa307d988860b14aac0f8fd7f928436df454529b
footoredo <footoredo@gmail.com>
2016-08-07 20:02:05 +0800

    add rotate for computational-geometry/2d/basis

--- Gungnir's Standard Library/source/computational-geometry/2d/basis.cpp
+++ Gungnir's Standard Library/source/computational-geometry/2d/basis.cpp
@@ -1,36 +1,43 @@
 typedef double DB;
 const DB eps = 1e-8;
 
-__inline int sign(DB x) {
+int sign(DB x) {
 	return x < -eps ? -1 : ( x > eps ? 1 : 0 );
 }
-__inline DB msqrt(DB x) {
+DB msqrt(DB x) {
 	return sign(x) > 0 ? sqrt(x) : 0;
 }
 
 struct Point {
 	DB x, y;
-	__inline Point(): x(0), y(0) {}
-	__inline Point(DB x, DB y): x(x), y(y) {}
-	__inline Point operator+(const Point &rhs) const {
+	Point(): x(0), y(0) {}
+	Point(DB x, DB y): x(x), y(y) {}
+	Point operator+(const Point &rhs) const {
 		return Point(x + rhs.x, y + rhs.y);
 	}
-	__inline Point operator-(const Point &rhs) const {
+	Point operator-(const Point &rhs) const {
 		return Point(x - rhs.x, y - rhs.y);
 	}
-	__inline Point operator*(DB k) const {
+	Point operator*(DB k) const {
 		return Point(x * k, y * k);
 	}
-	__inline Point operator/(DB k) const {
+	Point operator/(DB k) const {
 		assert(sign(k));
 		return Point(x / k, y / k);
 	}
+	Point rotate(DB ang) const {  // 逆时针旋转 ang 弧度
+		return Point(cos(ang) * x - sin(ang) * y, 
+				cos(ang) * y + sin(ang) * x);
+	}
+	Point turn90() const {  // 逆时针旋转 90 度
+		return Point(-y, x);
+	}
 };
 
-__inline DB dot(const Point& a, const Point& b) {
+DB dot(const Point& a, const Point& b) {
 	return a.x * b.x + a.y * b.y;
 }
 
-__inline DB det(const Point& a, const Point& b) {
+DB det(const Point& a, const Point& b) {
 	return a.x * b.y - a.y * b.x;
 }
